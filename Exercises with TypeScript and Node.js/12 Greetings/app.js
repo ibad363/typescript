d158ab8ba4170c5d367590d915bf57e4d9b044fa
Ibad Ur Rehman <HAFIZIBADURREHMAN363@GMAIL.COM>
2024-02-21 10:15:12 +0500

    ex12 greeting msg complete

--- Exercises with TypeScript and Node.js/12 Greetings/app.js
+++ Exercises with TypeScript and Node.js/12 Greetings/app.js
@@ -1,14 +1,12 @@
+"use strict";
 // Greetings: Start with the array you used in Exercise 11, but instead of just printing each person’s name, print a message to them. The text of each message should be the same, but each message should be personalized with the person’s name.
-
-let names: string[] = ['Farhan Bhai','Rayyan Bhai','Jahanzaib','Abdul Rafay Bhai','Ibad']
-
+let names = ['Farhan Bhai', 'Rayyan Bhai', 'Jahanzaib', 'Abdul Rafay Bhai', 'Ibad'];
 // one by one method
 console.log(`Welcome to Ibad Coding! ${names[0]}`);
 console.log(`Welcome to Ibad Coding! ${names[1]}`);
 console.log(`Welcome to Ibad Coding! ${names[2]}`);
 console.log(`Welcome to Ibad Coding! ${names[3]}`);
 console.log(`Welcome to Ibad Coding! ${names[4]}`);
-
 // //by loop method
 // for(let i = 0;i<names.length;i++){
 //   console.log(`Welcome to Ibad Coding! ${names[i]}`)

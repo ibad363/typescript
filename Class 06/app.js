4f70b3d4ab4ab595c03659b68f908837d3f53b06
Ibad Ur Rehman <HAFIZIBADURREHMAN363@GMAIL.COM>
2024-03-21 13:20:41 +0500

    class 6 little coding

---
+++ Class 06/app.js
@@ -0,0 +1,12 @@
+// class 06 about if else statements
+//else if statements
+var weather = "cloudy";
+if (weather === "raining") {
+    console.log("wear a raincoat");
+}
+else if (weather === "cloudy") {
+    console.log("wear a light jacket");
+}
+else {
+    console.log("wear sunglasses");
+}
---
+++ Class 06/app.ts
@@ -0,0 +1,13 @@
+// class 06 about if else statements
+
+//else if statements
+
+let weather = "cloudy"
+
+if (weather === "raining"){
+    console.log("wear a raincoat");    
+}else if (weather === "cloudy"){
+    console.log("wear a light jacket");    
+}else{
+    console.log("wear sunglasses");
+}

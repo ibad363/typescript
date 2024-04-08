d786b498872c3e820ec47843241af637224e21fa
Ibad Ur Rehman <HAFIZIBADURREHMAN363@GMAIL.COM>
2024-04-09 02:59:21 +0500

    ex 30 complete

---
+++ Exercises with TypeScript and Node.js/29 Favorite Fruit/app.ts
@@ -0,0 +1,25 @@
+// // Favorite Fruit: Make a array of your favorite fruits, and then write a series of independent if statements that check for certain fruits in your array.
+// • Make a array of your three favorite fruits and call it favorite_fruits.
+// • Write five if statements. Each should check whether a certain kind of fruit is in your array. If the fruit is in your array, the if block should print a statement, such as You really like bananas!
+
+let favFruits : string[] = ["banana","apple","mango"]
+
+if(favFruits.includes("banana")){
+  console.log("i like bananas")
+}
+
+if(favFruits.includes("apple")){
+  console.log("i like apples")
+}
+
+if(favFruits.includes("mango")){
+  console.log("i like mangoes") 
+}
+
+if(favFruits.includes("cherries")){
+  console.log("i like cherries")
+}
+
+if(favFruits.includes("watermelon")){
+  console.log("i like watermelon")
+}

75b67e51a24700e69b1de40e0e4a741c7a57d17d
Ibad Ur Rehman <HAFIZIBADURREHMAN363@GMAIL.COM>
2024-04-14 11:12:46 +0500

    ex 37 complete

---
+++ Exercises with TypeScript and Node.js/37 Large Shirts/app.js
@@ -0,0 +1,9 @@
+"use strict";
+// Large Shirts: Modify the make_shirt() function so that shirts are large by default with a message that reads I love TypeScript. Make a large shirt and a medium shirt with the default message, and a shirt of any size with a different message.
+function make_shirt(size = "large", message = "I love Typescript") {
+    console.log(`making the "${size}" shirt with the message "${message}" printed on it.`);
+}
+make_shirt(); // default size & message
+make_shirt("medium"); //default message & medium size
+make_shirt(undefined, "I love python"); //default size & custom message
+make_shirt("small", "I love python"); //custom size & message

0b755eba678c37eb143bea89ba5a36e2c41dcebb
Ibad Ur Rehman <HAFIZIBADURREHMAN363@GMAIL.COM>
2024-04-20 11:54:45 +0500

    project 00 code

--- Projects with TypeScript and Node.js/project00_simple-calculator/index.js
+++ Projects with TypeScript and Node.js/project00_simple-calculator/index.js
@@ -1,17 +1,20 @@
 import inquirer from "inquirer";
-
 const answer = await inquirer.prompt([
     { message: "Your First Number", type: "number", name: "firstNumber" },
     { message: "Your Second Number", type: "number", name: "secondNumber" },
     { message: "Select One of the Operation to Perform Actions", type: "list", name: "operation", choices: ["Addition", "Subtraction", "Multiplication", "Division"] }
-])
-
+]);
 if (answer.operation === "Addition") {
     console.log("Your Answer is Here: " + (answer.firstNumber + answer.secondNumber));
-} else if (answer.operation === "Subtraction") {
+}
+else if (answer.operation === "Subtraction") {
     console.log(`Your Answer is Here: ${answer.firstNumber - answer.secondNumber}`);
-} else if (answer.operation === "Multiplication") {
+}
+else if (answer.operation === "Multiplication") {
     console.log(`Your Answer is Here: ${answer.firstNumber * answer.secondNumber}`);
-} else if (answer.operation === "Division") {
+}
+else if (answer.operation === "Division") {
     console.log(`Your Answer is Here: ${answer.firstNumber / answer.secondNumber}`);
-} else (`Select Valid Operation`)
+}
+else
+    (`Select Valid Operation`);

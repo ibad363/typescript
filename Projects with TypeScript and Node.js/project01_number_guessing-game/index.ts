6358758b4c699f54d87b4e489363fbb7059613f7
Ibad Ur Rehman <HAFIZIBADURREHMAN363@GMAIL.COM>
2024-04-29 12:49:56 +0500

    project 01 (number guessing) completed

--- Projects with TypeScript and Node.js/project01_number_guessing-game/index.ts
+++ Projects with TypeScript and Node.js/project01_number_guessing-game/index.ts
@@ -1,3 +1,17 @@
+#! /usr/bin/env node
+
 import inquirer from "inquirer"
 
-const answer =  await inquirer.prompt()
+const randomNumber = Math.floor(Math.random()* 6 + 1)
+
+console.log("Welcome to Number Guessing Game");
+
+const answer =  await inquirer.prompt(
+    {message: "Please Guess a Number between 1-6",type: "number",name: "userGuess"}
+)
+
+if(randomNumber === answer.userGuess){
+    console.log("Congratulations! You Guess the Right Number");
+}else{
+    console.log("You Guess Wrong Number");
+}

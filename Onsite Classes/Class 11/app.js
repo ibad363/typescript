5d3677d78ec335bf8768d51de205b2bb19977795
Ibad Ur Rehman <HAFIZIBADURREHMAN363@GMAIL.COM>
2024-05-13 00:18:29 -0700

    class 11 9/5/24

---
+++ Onsite Classes/Class 11/app.js
@@ -0,0 +1,28 @@
+// in class 11, array, array methods and object
+var fruits = ["apple", "banana", "mango", "pineapple"];
+// fruits.pop() // remove from end
+// fruits.shift() // remove from start
+// fruits.push("Orange") //add in end
+// // fruits.unshift("Papita") //add in start
+// let extract = fruits.slice(1,3) //extracts from original array
+// console.log(extract);
+// fruits.splice(1,1,"Cheeku") //add element by index number and also delete element if you want
+// console.log(fruits);
+var student = {
+    name: "Ibad",
+    rollno: 21541616,
+    Timing: "7 to 10"
+};
+console.log(student);
+console.log(student.name);
+var student2 = [
+    { name: "Ibad",
+        rollno: 21541616,
+        Timing: "7 to 10"
+    },
+    {
+        name: "Rayyan",
+        rollNo: 4103315,
+        Timing: "7 to 10"
+    }
+];
---
+++ Onsite Classes/Class 11/app.ts
@@ -0,0 +1,36 @@
+// in class 11, array, array methods and object
+
+let fruits : string [] = ["apple", "banana" , "mango", "pineapple"]
+// fruits.pop() // remove from end
+
+// fruits.shift() // remove from start
+
+// fruits.push("Orange") //add in end
+
+// // fruits.unshift("Papita") //add in start
+
+// let extract = fruits.slice(1,3) //extracts from original array
+// console.log(extract);
+
+// fruits.splice(1,1,"Cheeku") //add element by index number and also delete element if you want
+// console.log(fruits);
+
+let student= {
+    name : "Ibad",
+    rollno: 21541616,
+    Timing: "7 to 10"
+}
+console.log(student);
+console.log(student.name);
+
+let student2 = [
+    {name : "Ibad",
+    rollno: 21541616,
+    Timing: "7 to 10"
+    },
+    {
+        name: "Rayyan",
+        rollNo: 4103315,
+        Timing: "7 to 10"
+    }
+]

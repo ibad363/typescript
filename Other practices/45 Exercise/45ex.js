a65245397d1ea6d39ed6627d95f9d4ba3f7d684e
Ibad Ur Rehman <HAFIZIBADURREHMAN363@GMAIL.COM>
2024-07-14 23:53:45 +0500

    34 EX COMPLETE

--- Other practices/45 Exercise/45ex.js
+++ Other practices/45 Exercise/45ex.js
@@ -400,51 +400,67 @@
 // 31. No Users: Add an if test to Exercise 28 to make sure the list of users is not empty.
 // • If the list is empty, print the message We need to find some users!
 // • Remove all of the usernames from your array, and make sure the correct message is printed.
+// let username: string[] = ["user1","user2","user3","user4","user5","admin"]
+// // for(let i=0;i<6;i++){
+// //     username.pop()
+// // }
+// // while(username.length>0){
+// //     username.pop()
+// // }
+// // username.splice(0,username.length)
+// username.length = 0
+// if(username.length === 0){
+//     console.log("We need to find some users!");
+// }else{
+//     console.log("we have user in list");
+// }
+// ex 31 complete
+// 32. Checking Usernames: Do the following to create a program that simulates how websites ensure that everyone has a unique username.
+// • Make a list of five or more usernames called current_users.
+// • Make another list of five usernames called new_users. Make sure one or two of the new usernames are also in the current_users list.
+// • Loop through the new_users list to see if each new username has already been used. If it has, print a message that the person will need to enter a new username. If a username has not been used, print a message saying that the username is available.
+// • Make sure your comparison is case insensitive. If 'John' has been used, 'JOHN' should not be accepted.
+// let currentUsers: string[] = ["user1","uSer2","user3","user4","user5"]
+// let newUsers: string[] = ["user6","User2","user8","user9","user5"]
+// newUsers.forEach(newuser=>{
+//     if(currentUsers.some(current=>current.toLowerCase() === newuser.toLowerCase())){
+//         console.log(`the ${newuser} will need to enter a new username.`);
+//     }else{
+//         console.log(`username is available.`);
+//     }
+// })
+// ex 32 complete
+// 33. Ordinal Numbers: Ordinal numbers indicate their position in a array, such as 1st or 2nd. Most ordinal numbers end in th, except 1, 2, and 3.
+// • Store the numbers 1 through 9 in a array.
+// • Loop through the array.
+// • Use an if-else chain inside the loop to print the proper ordinal ending for each number. Your output should read "1st 2nd 3rd 4th 5th 6th 7th 8th 9th", and each result should be on a separate line.
+// let number :number[] = [1,2,3,4,5,6,7,8,9]
+// number.forEach(num=>{
+//     let suffix = "th"
+//     if(num == 1){
+//         suffix = "st"
+//     }else if(num == 2){
+//         suffix = "nd"
+//     }else if(num == 3){
+//         suffix = "rd"
+//     }
+//     console.log(`${num}${suffix}`);
+// })
+// ex 33 complete
+// 34. Pizzas: Think of at least three kinds of your favorite pizza. Store these pizza names in a array, and then use a for loop to print the name of each pizza.
+// • Modify your for loop to print a sentence using the name of the pizza instead of printing just the name of the pizza. For each pizza you should have one line of output containing a simple statement like I like pepperoni pizza.
+// • Add a line at the end of your program, outside the for loop, that states how much you like pizza. The output should consist of three or more lines about the kinds of pizza you like and then an additional sentence, such as I really love pizza!
+// let favPizza :string[] = ["Pizza 1","Pizza 2","Pizza 3","Pizza 4","Pizza 5"]
+// favPizza.forEach(pizza=>{
+//     console.log(`i like ${pizza} pizza`);
+// })
+// console.log("i really love pizza!");
+// ex 34 complete
+// 35. Animals: Think of at least three different animals that have a common characteristic.
+// Store the names of these animals in a list, and then use a for loop to print out the name of each animal.
+// • Modify your program to print a statement about each animal, such as A dog would make a great pet.
+// • Add a line at the end of your program stating what these animals have in common. You could print a sentence such as Any of these animals would make a great pet!
 /*
-32. Checking Usernames: Do the following to create a program that simulates how websites ensure that everyone has a unique username.
-
-• Make a list of five or more usernames called current_users.
-
-• Make another list of five usernames called new_users. Make sure one or two of the new usernames are also in the current_users list.
-
-• Loop through the new_users list to see if each new username has already been used. If it has, print a message that the person will need to enter a
-new username. If a username has not been used, print a message saying that the username is available.
-
-• Make sure your comparison is case insensitive. If 'John' has been used, 'JOHN' should not be accepted.
-
-33. Ordinal Numbers: Ordinal numbers indicate their position in a array, such as 1st or 2nd. Most ordinal numbers end in th, except 1, 2, and 3.
-
-• Store the numbers 1 through 9 in a array.
-
-• Loop through the array.
-
-• Use an if-else chain inside the loop to print the proper ordinal ending for each number. Your output should read "1st 2nd 3rd 4th 5th 6th
-7th 8th 9th", and each result should be on a separate line.
-
-
-34. Pizzas: Think of at least three kinds of your favorite pizza. Store these
-pizza names in a array, and then use a for loop to print the name of each pizza.
-
-• Modify your for loop to print a sentence using the name of the pizza
-instead of printing just the name of the pizza. For each pizza you should
-have one line of output containing a simple statement like I like pepperoni
-pizza.
-
-• Add a line at the end of your program, outside the for loop, that states
-how much you like pizza. The output should consist of three or more lines
-about the kinds of pizza you like and then an additional sentence, such as
-I really love pizza!
-
-35. Animals: Think of at least three different animals that have a common characteristic.
-Store the names of these animals in a list, and then use a for loop to
-print out the name of each animal.
-• Modify your program to print a statement about each animal, such as
-A dog would make a great pet.
-• Add a line at the end of your program stating what these animals have in
-common. You could print a sentence such as Any of these animals would
-make a great pet!
-
-
 36. T-Shirt: Write a function called make_shirt() that accepts a size and the
 text of a message that should be printed on the shirt. The function should print
 a sentence summarizing the size of the shirt and the message printed on it.

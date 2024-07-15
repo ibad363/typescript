13ca233a0462cd26c07968dfed5c2a86a4591ee7
Ibad Ur Rehman <HAFIZIBADURREHMAN363@GMAIL.COM>
2024-07-15 12:30:40 +0500

    45 ex continue

--- Other practices/45 Exercise/45ex.js
+++ Other practices/45 Exercise/45ex.js
@@ -460,71 +460,122 @@
 // Store the names of these animals in a list, and then use a for loop to print out the name of each animal.
 // • Modify your program to print a statement about each animal, such as A dog would make a great pet.
 // • Add a line at the end of your program stating what these animals have in common. You could print a sentence such as Any of these animals would make a great pet!
-/*
-36. T-Shirt: Write a function called make_shirt() that accepts a size and the
-text of a message that should be printed on the shirt. The function should print
-a sentence summarizing the size of the shirt and the message printed on it.
-Call the function.
-
-37. Large Shirts: Modify the make_shirt() function so that shirts are large
-by default with a message that reads I love TypeScript. Make a large shirt and a
-medium shirt with the default message, and a shirt of any size with a different
-message.
-
-38. Cities: Write a function called describe_city() that accepts the name of
-a city and its country. The function should print a simple sentence, such as
-Karachi is in Pakistan. Give the parameter for the country a default value.
-Call your function for three different cities, at least one of which is not in the
-default country.
-
-39. City Names: Write a function called city_country() that takes in the name
-of a city and its country. The function should return a string formatted like this:
-
-"Lahore, Pakistan"
-
-Call your function with at least three city-country pairs, and print the value
-that’s returned.
-
-
-40. Album: Write a function called make_album() that builds a Object
-describing a music album. The function should take in an artist name and an
-album title, and it should return a Object containing these two pieces of
-information. Use the function to make three dictionaries representing different
-albums. Print each return value to show that Objects are storing the
-album information correctly.
-Add an optional parameter to make_album() that allows you to store the
-number of tracks on an album. If the calling line includes a value for the number
-of tracks, add that value to the album’s Object. Make at least one new
-function call that includes the number of tracks on an album.
-
-
-41. Magicians: Make a array of magician’s names. Pass the array to a function
-called show_magicians(), which prints the name of each magician in the array.
-
-42. Great Magicians: Start with a copy of your program from Exercise 39.
-Write a function called make_great() that modifies the array of magicians by adding
-the phrase the Great to each magician’s name. Call show_magicians() to
-see that the list has actually been modified.
-
-43. Unchanged Magicians: Start with your work from Exercise 40. Call the
-function make_great() with a copy of the array of magicians’ names. Because the
-original array will be unchanged, return the new array and store it in a separate array.
-Call show_magicians() with each array to show that you have one array of the original
-names and one array with the Great added to each magician’s name.
-
-
-44. Sandwiches: Write a function that accepts a array of items a person wants
-on a sandwich. The function should have one parameter that collects as many
-items as the function call provides, and it should print a summary of the sandwich
-that is being ordered. Call the function three times, using a different number
-of arguments each time.
-
-
-45. Cars: Write a function that stores information about a car in a Object.
-The function should always receive a manufacturer and a model name. It
-should then accept an arbitrary number of keyword arguments. Call the function
-with the required information and two other name-value pairs, such as a
-color or an optional feature.
-Print the Object that’s returned to make sure all the information was
-stored correctly.
-*/ 
+// let animals : string[] = ["Cat","Dog","Cow","Rabbit"]
+// animals.forEach(animal=>{
+//     console.log(`A ${animal} would make a great pet.`);
+// })
+// console.log("Any of these animals would make a great pet!");
+// ex 35 complete
+// 36. T-Shirt: Write a function called make_shirt() that accepts a size and the text of a message that should be printed on the shirt. The function should print a sentence summarizing the size of the shirt and the message printed on it. Call the function.
+// function make_shirt(size: string, text:string){
+//     console.log(`Make Shirt With Size '${size}' and Message '${text}'   `);
+// }
+// make_shirt("Medium","Coding is Passion")
+// ex 36 complete
+// 37. Large Shirts: Modify the make_shirt() function so that shirts are large by default with a message that reads I love TypeScript. Make a large shirt and a medium shirt with the default message, and a shirt of any size with a different message.
+// function make_shirt(size: string = "large", text:string = "I love TypeScript"){
+//     console.log(`Make Shirt With Size '${size}' and Message '${text}'   `);
+// }
+// make_shirt()
+// make_shirt("medium")
+// make_shirt("Small","I am AI Engineer")
+// make_shirt(undefined,"I am AI Engineer")
+// ex 37 complete
+// 38. Cities: Write a function called describe_city() that accepts the name of a city and its country. The function should print a simple sentence, such as Karachi is in Pakistan. Give the parameter for the country a default value. Call your function for three different cities, at least one of which is not in the default country.
+// function describe_city(city:string,country:string = "Pakistan"){
+//     console.log(`${city} is in ${country}`);
+// }
+// describe_city("Karachi")
+// describe_city("Lahore")
+// describe_city("Islamabad")
+// describe_city("Mumbai","India")
+// ex 38 complete
+// 39. City Names: Write a function called city_country() that takes in the name of a city and its country. The function should return a string formatted like this:
+// "Lahore, Pakistan"
+// Call your function with at least three city-country pairs, and print the value that’s returned.
+// function city_country(city:string,country: string){
+//     return `${city}, ${country}`
+// }
+// console.log(city_country("Karachi","Pakistan"));
+// console.log(city_country("Lahore","Pakistan"));
+// console.log(city_country("Islamabad","Pakistan"));
+// console.log(city_country("Delhi","India"));
+// // ex 39 complete
+// 40. Album: Write a function called make_album() that builds a Object describing a music album. The function should take in an artist name and an album title, and it should return a Object containing these two pieces of information. Use the function to make three dictionaries representing different albums. Print each return value to show that Objects are storing the album information correctly.
+// Add an optional parameter to make_album() that allows you to store the number of tracks on an album. If the calling line includes a value for the number of tracks, add that value to the album’s Object. Make at least one new function call that includes the number of tracks on an album.
+// function make_album(artistName: string, albTitle:string, tracks?:number){
+//     let album :{artistName : string, albumTitle:string,tracks?:number} = {
+//         artistName: artistName,
+//         albumTitle: albTitle
+//     }
+//     if(tracks != undefined){
+//         album.tracks = tracks
+//     }
+//     return album
+// }
+// console.log(make_album("Artist 1","Album 1"));
+// console.log(make_album("Artist 2","Album 2"));
+// console.log(make_album("Artist 3","Album 3"));
+// console.log(make_album("Artist 4","Album 4",10));
+// ex 40 complete
+// 41. Magicians: Make a array of magician’s names. Pass the array to a function called show_magicians(), which prints the name of each magician in the array.
+// let magician :string[] = ["Magician 1","Magician 2","Magician 3","Magician 4"]
+// function show_magicians (magician:string[]){
+//     magician.forEach(magician=>{
+//         console.log(magician);
+//     })
+// }
+// show_magicians(magician)
+// ex 41 complete
+// 42. Great Magicians: Start with a copy of your program from Exercise 39. Write a function called make_great() that modifies the array of magicians by adding the phrase the Great to each magician’s name. Call show_magicians() to see that the list has actually been modified.
+// let magician :string[] = ["Magician 1","Magician 2","Magician 3","Magician 4"]
+// function show_magicians (array:string[]){
+//     array.forEach(magician=>{
+//         console.log(magician);
+//     })
+// }
+// function make_great(array:string[]){
+//     array.forEach(magician=>{
+//         console.log(`The Great ${magician}`);
+//     })
+// }
+// show_magicians(magician)
+// make_great(magician)
+// ex 42 complete
+// 43. Unchanged Magicians: Start with your work from Exercise 40. Call the function make_great() with a copy of the array of magicians’ names. Because the original array will be unchanged, return the new array and store it in a separate array.
+// Call show_magicians() with each array to show that you have one array of the original names and one array with the Great added to each magician’s name.
+// let magician :string[] = ["Magician 1","Magician 2","Magician 3","Magician 4"]
+// function show_magicians (array:string[]){
+//     array.forEach(magician=>{
+//         console.log(magician);
+//     })
+// }
+// function make_great(array:string[]){
+//     array.forEach(magician=>{
+//         console.log(`The Great ${magician}`);
+//     })
+// }
+// let copyMagician:string[] = [...magician]
+// console.log("This is Original Array");
+// show_magicians(magician)
+// console.log("This is Modified Array");
+// make_great(copyMagician)
+// ex 43 complete
+// 44. Sandwiches: Write a function that accepts a array of items a person wants on a sandwich. The function should have one parameter that collects as many items as the function call provides, and it should print a summary of the sandwich that is being ordered. Call the function three times, using a different number of arguments each time.
+// function makeSandwich(...item:string[]){
+//     console.log(`Make Sandwich with ${item.join(", ")}`);
+// }
+// makeSandwich("ham", "cheese")
+// makeSandwich("turkey", "lettuce", "tomato")
+// makeSandwich("avocado", "sprouts", "mustard", "mayo")
+// ex 44 complete
+// 45. Cars: Write a function that stores information about a car in a Object. The function should always receive a manufacturer and a model name. It should then accept an arbitrary number of keyword arguments. Call the function with the required information and two other name-value pairs, such as a color or an optional feature. 
+// Print the Object that’s returned to make sure all the information was stored correctly.
+function makeCar(manufacturer, modelName, ...extraOption) {
+    let car = Object.assign({ manufacturer: manufacturer, modelName: modelName }, Object.assign({}, ...extraOption));
+    return car;
+}
+console.log(makeCar("Honda", "Civic", { color: "Black", year: 2024 }));
+console.log(makeCar("Honda", "Civic", { color: "Black" }, { year: 2024 }));
+//"Honda","Civic", {color: "Black"}, {features: ["navigation", "window screen"]}))
+// "Ford","Fiesta", {color: "Blue"}, {Year: 2020}))
